14a67d9cbe7030eedd531da71aeb129a35b00a9e
senpai-10 <bmjfdrh@gmail.com>
2021-08-22 22:53:48 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(8);
+  const [checkboxes, setCheckboxes] = useState({
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    symbols: true,
+  });
+
+  function generate() {
+    let chars = "";
+    let pass = "";
+
+    if (checkboxes.uppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (checkboxes.lowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+    if (checkboxes.numbers) chars += "0123456789";
+
+    console.log(chars);
+
+    // return pass;
+  }
+
+  function handleChange(e: any) {
+    setLength(e.target.value);
+  }
+
+  function handleCheckbox(e: any) {
+    setCheckboxes({
+      ...checkboxes,
+      [e.target.name]: !!e.target.checked,
+    } as typeof checkboxes);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Password Generator</h2>
+      <div className="result-container">
+        <span id="result">{password}</span>
+        <button className="btn" id="clipboard">
+          <i className="far fa-clipboard"></i>
+        </button>
+      </div>
+      <div className="settings">
+        <div className="setting">
+          <label>Password length: {length}</label>
+          <input
+            onChange={(e) => handleChange(e)}
+            type="range"
+            min="8"
+            max="80"
+            value={length}
+          />
+        </div>
+        <div className="setting">
+          <label>Include uppercase letters</label>
+          <input
+            onClick={(e) => handleCheckbox(e)}
+            type="checkbox"
+            name="uppercase"
+            defaultChecked={checkboxes.uppercase}
+          />
+        </div>
+        <div className="setting">
+          <label>Include lowercase letters</label>
+          <input
+            onClick={(e) => handleCheckbox(e)}
+            type="checkbox"
+            name="lowercase"
+            defaultChecked={checkboxes.lowercase}
+          />
+        </div>
+        <div className="setting">
+          <label>Include numbers</label>
+          <input
+            onClick={(e) => handleCheckbox(e)}
+            type="checkbox"
+            name="numbers"
+            defaultChecked={checkboxes.numbers}
+          />
+        </div>
+        <div className="setting">
+          <label>Include symbols</label>
+          <input
+            onClick={(e) => handleCheckbox(e)}
+            type="checkbox"
+            name="symbols"
+            defaultChecked={checkboxes.symbols}
+          />
+        </div>
+      </div>
+      <button onClick={generate} className="btn btn-large" id="generate">
+        Generate password
+      </button>
     </div>
   );
 }
